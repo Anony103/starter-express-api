e1d655d2b994be9ab25b7fc87010dbc1cc91331e
Abdulfatai Ajani <99477371+Anony103@users.noreply.github.com>
2023-10-26 16:36:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -82,3 +82,46 @@ transporter.verify((error) => {
       }
     });
   });
+
+
+  router.all("/book", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+    const ticketType = req.body.ticketType; 
+        const mail = {
+          from: email,
+          to: useremail,
+          subject: subject,
+          html: `
+          <div style="width: 100%; background-color: #f3f9ff; padding: 5rem 0">
+          <div style="max-width: 700px; background-color: white;border: 2px solid #C4C4C4;border-radius:20px; margin: 0 auto">
+          <div style="width: 100%; height: 100px; border-radius:20px 20px 0px 0px; justify-content: center;text-align: center;">
+          <a href="http://www.r16services.com/" target="_blank"><img
+              src="https://firebasestorage.googleapis.com/v0/b/root16-3979e.appspot.com/o/root_16-removebg-preview.png?alt=media&token=75e81206-ee9d-4fc1-b310-23e4abe162e5"
+              style="width: 150px; height: 150px; "
+            /></a>
+          </div>
+            <div style="width: 100%; gap: 10px; padding: 30px 0; display: grid">
+              <p style="font-weight: 800; font-size: 1.2rem; padding: 0 30px">
+                ROOT16 feedback Form
+              </p>
+              <div style="font-size: .8rem; margin: 0 30px">
+                <p>Name: <b>${name}</b></p>
+                <p>Email: <b>${email}</b></p>
+                <p style="font-size: 20px;"><i>${ticketType}</i></p>
+              </div>
+            </div>
+          </div>
+        </div>
+    `,
+        };
+      transporter.sendMail(mail, (error) => {
+        if (error) {
+          res.json({ status: "ERROR" });
+        } else {
+          res.json({ status: "Message Sent" });
+        }
+      });
+    
+    });
